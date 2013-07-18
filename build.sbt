bbf7e923915228eb1d1a3f3e5d1fa92b6851aeb8
Andy Konwinski <andyk@berkeley.edu>
2013-07-18 14:23:07 -0700

    Updates to python test framework - runs but output needs cleanup.

--- build.sbt
+++ build.sbt
@@ -13,3 +13,8 @@ libraryDependencies += "net.sf.jopt-simple" % "jopt-simple" % "4.5"
 libraryDependencies += "org.scalatest" % "scalatest_2.9.2" % "1.8" % "test"
 
 libraryDependencies += "com.google.guava" % "guava" % "14.0.1"
+
+unmanagedJars in Compile <++= baseDirectory map  { base =>
+  val finder: PathFinder = (file("spark")) ** "*.jar"
+  finder.get
+}
--- src/main/scala/spark/perf/TestRunner.scala
+++ src/main/scala/spark/perf/TestRunner.scala
@@ -4,8 +4,12 @@ import spark.SparkContext
 
 object TestRunner {
   def main(args: Array[String]) {
-    val testName = args(1)
-    val master = args(2)
+    if (args.size < 4) {
+      println("TestRunner requires 4 or more args, you gave %s, exiting".format(args.size))
+      System.exit(1)
+    }
+    val master = args(1)
+    val testName = args(2)
     val otherArgs = args.slice(3, args.length)
     val sc = new SparkContext(master, "Test Runner: " + testName, null,
       Seq("./target/scala-2.9.3/spark-perf_2.9.3-0.1.jar"))
