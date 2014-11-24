5a289699bafc794788398c1bf7b7ab7a962f1a96
Joseph K. Bradley <joseph@databricks.com>
2014-11-24 13:06:21 -0800

    updated config for mllib

--- lib/sparkperf/main.py
+++ lib/sparkperf/main.py
@@ -42,15 +42,15 @@ run_spark_tests = config.RUN_SPARK_TESTS and (len(config.SPARK_TESTS) > 0)
 run_pyspark_tests = config.RUN_PYSPARK_TESTS and (len(config.PYSPARK_TESTS) > 0)
 run_streaming_tests = config.RUN_STREAMING_TESTS and (len(config.STREAMING_TESTS) > 0)
 run_mllib_tests = config.RUN_MLLIB_TESTS and (len(config.MLLIB_TESTS) > 0)
-run_pyspark_mllib_tests = config.RUN_PYSPARK_MLLIB_TESTS and (len(config.PYSPARK_MLLIB_TESTS) > 0)
+run_python_mllib_tests = config.RUN_PYTHON_MLLIB_TESTS and (len(config.PYTHON_MLLIB_TESTS) > 0)
 run_tests = run_spark_tests or run_pyspark_tests or run_streaming_tests or run_mllib_tests \
-    or run_pyspark_mllib_tests
+    or run_python_mllib_tests
 
 # Only build the perf test sources that will be used.
 should_prep_spark = not config.USE_CLUSTER_SPARK and config.PREP_SPARK
 should_prep_spark_tests = run_spark_tests and config.PREP_SPARK_TESTS
 should_prep_streaming_tests = run_streaming_tests and config.PREP_STREAMING_TESTS
-should_prep_mllib_tests = (run_mllib_tests or run_pyspark_mllib_tests) and config.PREP_MLLIB_TESTS
+should_prep_mllib_tests = (run_mllib_tests or run_python_mllib_tests) and config.PREP_MLLIB_TESTS
 
 # Do disk warmup only if there are tests to run.
 should_warmup_disk = run_tests and config.DISK_WARMUP
@@ -132,9 +132,9 @@ if run_pyspark_tests:
     PythonTests.run_tests(cluster, config, config.PYSPARK_TESTS, "PySpark-Tests",
                           config.PYSPARK_OUTPUT_FILENAME)
 
-if run_pyspark_mllib_tests:
-    PythonTests.run_tests(cluster, config, config.PYSPARK_MLLIB_TESTS, "PySpark-MLlib-Tests",
-                          config.PYSPARK_MLLIB_OUTPUT_FILENAME)
+if run_python_mllib_tests:
+    PythonTests.run_tests(cluster, config, config.PYTHON_MLLIB_TESTS, "Python-MLlib-Tests",
+                          config.PYTHON_MLLIB_OUTPUT_FILENAME)
 
 if run_streaming_tests:
     StreamingTests.run_tests(cluster, config, config.STREAMING_TESTS, "Streaming-Tests",
