91842b131c3da72280dd1ed58423c3c8a3389eb2
Nicholas Chammas <nicholas.chammas@gmail.com>
2015-02-02 13:55:55 -0800

    Protect against word splitting in dirname call

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-export PYTHONPATH="$(dirname $0)/lib/:$PYTHONPATH"
-nosetests --with-doctest -v "$(dirname $0)/lib/sparkperf"
+export PYTHONPATH="$(dirname "$0")/lib/:$PYTHONPATH"
+nosetests --with-doctest -v "$(dirname "$0")/lib/sparkperf"
