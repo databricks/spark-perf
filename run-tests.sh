e72bc0990e5d89829b4cc2863dadc9b2ffffefd1
Xiangrui Meng <meng@databricks.com>
2015-02-23 11:44:22 -0800

    update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-export PYTHONPATH="$(dirname "$0")/lib/:$PYTHONPATH"
-nosetests --with-doctest -v "$(dirname "$0")/lib/sparkperf"
+export PYTHONPATH="$(dirname "$0")/lib/:$HOME/lib/site-python:$PYTHONPATH"
+/usr/bin/env python -c "from nose import main; main()" --with-doctest -v "$(dirname "$0")/lib/sparkperf"
